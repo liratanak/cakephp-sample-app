5a40bec29f8b70881cc3df39b15610fbc0242af1
Liratanak <liratanak@web-essentials.asia>
2013-11-29 11:14:57 +0700

    [TASK] cake bake all

--- app/Config/database.php
+++ app/Config/database.php
@@ -65,11 +65,11 @@ class DATABASE_CONFIG {
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'database_name',
+		'login' => 'root',
+		'password' => 'ess:3',
+		'database' => 'spd_cakephp241',
 		'prefix' => '',
-		//'encoding' => 'utf8',
+		'encoding' => 'utf8',
 	);
 
 	public $test = array(
